4178f833e13fdae3cb384940722966d45edf08e2
acybulska <aleksandralisacybulska@gmail.com>
2019-05-28 11:25:53 +0200

    Finished Assignment 2

--- Assignment2/app.js
+++ Assignment2/app.js
@@ -1,6 +1,14 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
+    el: '#exercise',
+    data: {
+        value: ''
+    },
+    methods: {
+        getAlert: function() {
+            alert('Alert');
+        },
+        getValue: function (event) {
+            this.value = event.target.value;
         }
-    });
+    }
+});
