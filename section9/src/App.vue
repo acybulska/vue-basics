4bce56de3cf55823d7c29520eca965dd12cef1b3
Lisa Aleksandra Cybulska <acybulska@Aleksandras-MacBook-Pro.local>
2019-06-12 09:05:17 +0200

    Add quote via props

---
+++ section9/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-quote :quote="'A wonderful quote!'"/>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue'
+export default {
+  name: 'app',
+  components: {
+    'appQuote': Quote
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ section9/src/components/Quote.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>{{quote}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['quote']
+}
+</script>
+
+<style scoped>
+div {
+    border: solid 1px #ccc;
+    box-shadow: 1px 1px 2px black;
+    margin: 30px auto;
+    padding: 30px;
+    text-align: center;
+}
+</style>
